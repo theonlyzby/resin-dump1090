2d4399030f49d1b41e7e863e4fa4235f40d69622
Theonlyzby <theonlyzby@gmail.com>
2018-06-25 22:03:15 +0200

    Update start.sh

--- opt/start.sh
+++ opt/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-/srv/dump1090/dump1090 --net --no-fix --net-ro-size 500 --net-ro-rate 5 --net-heartbeat 60 --net-http-port 80 --lat LAT --lon LONG &
+cd /srv/dump1090
+./dump1090 --net --no-fix --net-ro-size 500 --net-ro-rate 5 --net-heartbeat 60 --net-http-port 80 --lat LAT --lon LONG &
 #  --gain -10 --interactive
 # To prevent Docker from exiting
 bash
