48eb710991ead1dcbd509d86f1ec86e2ac0b8023
Theonlyzby <theonlyzby@gmail.com>
2018-08-21 09:17:02 +0200

    startup pause - fix auto-start issue

--- opt/start.sh
+++ opt/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 60
 cd /srv/dump1090
 ./dump1090 --net --no-fix --net-ro-size 500 --net-ro-rate 5 --net-heartbeat 60 --net-http-port 80 --lat LAT --lon LONG &
 #  --gain -10 --interactive
